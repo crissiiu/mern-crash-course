e05cb91e1b70256868029cf7290112450fcd2a1a
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-03-21 20:38:28 +0700

    Done setup

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -52,11 +52,18 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted" });
   } catch (error) {
     console.log("Error in deleting product: ", error.message);
-    res.status(404).json({ success: false, message: "Product Notfound" });
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,15 @@ import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allow accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://locahost:5000");
+  console.log("Server started at http://locahost:" + PORT);
 });
