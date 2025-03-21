0d010924c090c8891b992b735270e890cc2210d9
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-03-21 17:37:39 +0700

    Done build product api

---
+++ README.md
@@ -0,0 +1,3 @@
+# MERN-CRASH-COURSE
+
+Ứng dụng đơn giản để bắt đầu với Mern, backend và frontend
---
+++ backend/routes/product.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+  deleteProduct,
+  getProducts,
+  newProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+
+router.post("/", newProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
+
+export default router;
